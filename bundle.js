43c9dbc5dac5651a1192c150a5930b1ff909a3dc
Abby McLeran <amcleran@pugetsound.edu>
2021-11-16 20:57:04 -0800

    code working besides waiting for user guesses

--- bundle.js
+++ bundle.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 // use api to play intro when webpage is loaded up
 // make functions that play the corresponding noise and light up when buttons are pressed
 // make buttons that light up when hovered over
@@ -273,3 +274,5 @@ async function turnCheck() {
         return;
     }
 }
+
+},{}]},{},[1]);
